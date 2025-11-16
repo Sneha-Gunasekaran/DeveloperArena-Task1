4d4a658f7bbf4c711cfd1487c76316c4d51a9edd
Sneha G <snehaguna13112005@gmail.com>
2025-11-16 22:08:39 +0530

    Task1 Finished

---
+++ Task1.py
@@ -0,0 +1,15 @@
+print("-----------PERSONAL INFORMATION MANAGER---------")
+
+name=input("Enter your Name:")
+age=input("Enter your Age:")
+city=input("Enter your City:")
+hobbies=input("Enter your Hobbies:")
+
+print("\n-----------------Your Details-----------------")
+
+print("Name     :", name)
+print("Age      :", age)
+print("City     :", city)
+print("Hobbies  :", hobbies)
+print("---------------------------------------------")
+print("Thank you for using the Personal Infermation Manager!\n")
